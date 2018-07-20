18d1e799be039e3a00e437d54bc3f4ddd2c92f58
Flavio Enrique <enrikeflavio@gmail.com>
2018-07-20 13:04:50 -0300

    criando componentes dos layouts

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutComponent } from './layout.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    SidebarComponent,
+    HeaderComponent,
+    ContainerComponent
   ]
 })
 export class LayoutModule { }
