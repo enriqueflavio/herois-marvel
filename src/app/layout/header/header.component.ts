e38e00015ff99a2822a688fef8a4a81a77a80810
Flavio Enrique <enrikeflavio@gmail.com>
2018-07-20 22:09:41 -0300

    animação do header e suas imagens iniciais

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  animate = false;
+  img;
+
   constructor() { }
 
   ngOnInit() {
+    this.randomImage();
+    this.esperaAnimate();
   }
+
+  esperaAnimate() {
+    setTimeout( () => {
+      this.animate = !this.animate;
+      this.animateNow();
+    }, 5000);
+  }
+
+  trocaImage() {
+    setTimeout( () => {
+      this.randomImage();
+      this.animateNow();
+    }, 1);
+  }
+
+  animateNow() {
+    setTimeout( () => {
+      this.img = Math.floor(Math.random() * 10);
+      this.animate = !this.animate;
+      this.esperaAnimate();
+    }, 1000);
+  }
+
+  randomImage() {
+    this.img = Math.floor(Math.random() * 10);
+  }
+
 
 }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -6,6 +6,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
 import { ContainerComponent } from './container/container.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   imports: [
@@ -16,7 +17,8 @@ import { FooterComponent } from './footer/footer.component';
     SidebarComponent,
     HeaderComponent,
     ContainerComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ]
 })
 export class LayoutModule { }
