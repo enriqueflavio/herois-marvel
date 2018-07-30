e164f6e0d75901b3e291d57594e26ffa0d501d9a
enriqueflavio <enrikeflavio@gmail.com>
2018-07-30 16:54:13 -0300

    Delete infosAPI.ts

--- src/app/shared/infosAPI.ts
+++
@@ -1,7 +0,0 @@
-export class InfosAPI {
-  public static url = 'https://gateway.marvel.com/v1/public/';
-  public static ts = '1';
-  public static apikey = '3542ad80580d8245721d8ec337e5378f';
-  public static hashkey = '09ee7912cc847184acc876029b8a48d2';
-}
-
